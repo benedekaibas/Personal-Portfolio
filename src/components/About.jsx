3517446438589d59399fad3e72e65edbf31e911d
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-31 12:36:11 -0500

    updated url

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,10 @@ const About = () => {
     <section id="about" className="py-16 px-6 max-w-5xl mx-auto">
       <h3 className="text-3xl font-semibold mb-6">About Me</h3>
       <p className="text-lg leading-relaxed">
-        I'm a graduating Computer Science student with experience in embedded systems, robotics, and deep learning. 
-        I'm the captain of a nationally ranked tennis team and currently lead an ROV research project to diagnose local bodies of water.
+        I am a third-year international student-athlete at Allegheny College. Thanks to a school scholarship, I am able to continue my tennis career 
+        and pursue my studies in the United States after my home country, Hungary. I have been interested in the world of computers and programming since 
+        high school, which is why I chose computer science as my major. I have already completed small projects and internships, but I am eager to learn 
+        new skills and expand my career in the field of computer science.
       </p>
     </section>
   );
