18b8a43f8bdc8b13315a47690fb2b26aa5fe3b1b
benedekaibas <kaibas.benedek02@gmail.com>
2025-03-31 13:00:40 -0500

    resume added & implementation of opening it in new window is correct

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,7 @@ const Navbar = () => {
           <li><a href="#projects">Projects</a></li>
           <li><a href="#experience">Experience</a></li>
           <li><a href="#contact">Contact</a></li>
-          <li><a href="/resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
-        </ul>
+          <li><a href="/resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>        </ul> {/* Closing tag added here */}
       </div>
     </nav>
   );
